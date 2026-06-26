8b328dca675a63948b923a43a7a36851e2afac0b
sakars <o.putaans@gmail.com>
2026-06-26 13:38:25 +0300

    Ethernet monitor for LLDP packets created

--- include/lldp_monitor.hh
+++ include/lldp_monitor.hh
@@ -10,10 +10,78 @@
 #include <unistd.h>
 #include <optional>
 #include <memory>
+#include <functional>
+#include <span>
 #include "event_handlers.hh"
 
 namespace ndisc
 {
+    class EthernetLldpMonitor final : public EventHandler
+    {
+    public:
+        using Callback = std::function<void(const sockaddr_ll &, std::span<const uint8_t>)>;
+
+    private:
+        int socket_fd_;
+        std::vector<uint8_t> message_buffer_;
+        Callback callback_;
+
+        EthernetLldpMonitor(int socket_fd, Callback callback) : socket_fd_(socket_fd), callback_(std::move(callback))
+        {
+            message_buffer_.resize(2);
+        }
+
+    public:
+        static std::unique_ptr<EthernetLldpMonitor> Create(int device_idx, Callback callback)
+        {
+            int socket_fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_LLDP));
+            if (socket_fd < 0)
+            {
+                return nullptr;
+            }
+            return std::make_unique<EthernetLldpMonitor>(EthernetLldpMonitor(socket_fd, std::move(callback)));
+        }
+
+        int GetSocket() const override
+        {
+            return socket_fd_;
+        }
+
+        void Call() override
+        {
+            sockaddr_ll link_layer_address{};
+            iovec message_iovec{};
+            message_iovec.iov_base = message_buffer_.data();
+            message_iovec.iov_len = message_buffer_.size();
+            msghdr message_header{};
+            message_header.msg_name = &link_layer_address;
+            message_header.msg_namelen = sizeof(link_layer_address);
+            message_header.msg_iov = &message_iovec;
+            message_header.msg_iovlen = 1;
+            message_header.msg_control = nullptr;
+            message_header.msg_controllen = 0;
+            message_header.msg_flags = 0;
+
+            ssize_t peek_packet_length = recvmsg(socket_fd_, &message_header, MSG_PEEK | MSG_TRUNC);
+
+            if (peek_packet_length > message_buffer_.size())
+            {
+                message_buffer_.resize(peek_packet_length);
+            }
+
+            message_iovec.iov_base = message_buffer_.data();
+            message_iovec.iov_len = message_buffer_.size();
+
+            ssize_t received_length = recvmsg(socket_fd_, &message_header, 0);
+
+            callback_(link_layer_address, std::span<const uint8_t>(message_buffer_.begin(), received_length));
+        }
+
+        uint32_t GetEvents() const override
+        {
+            return EPOLLIN;
+        }
+    };
     // class LldpMonitor
     // {
     //     int socket_fd_ = -1;
